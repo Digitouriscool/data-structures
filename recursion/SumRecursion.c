c3b67a094faad3565f25274c13169ce465c89e45
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-05-13 19:21:13 -0600

    Add recursion code snippets.

---
+++ recursion/SumRecursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    return sum(n - 1) + n;
+}
+
+int Isum(int n)
+{
+    int s = 0;
+    int i;
+    for (i = 1; i <= n; i++)
+        s = s + i;
+    return s;
+}
+
+int main()
+{
+    int r;
+
+    // r = sum(5);  // recurisve
+    r = Isum(5); // iterative
+    printf("%d ", r);
+
+    return 0;
+}
---
+++ recursion/factorialRecursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    return fact(n - 1) * n;
+}
+
+int Ifact(int n)
+{
+    int f = 1;
+    int i;
+    for (i = 1; i <= n; i++)
+        f = f * i;
+    return f;
+}
+
+int main()
+{
+    int r;
+
+    // r = fact(5); // recursive
+    r = Ifact(5); // iterative
+    printf("%d ", r);
+
+    return 0;
+}
