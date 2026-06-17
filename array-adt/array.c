7968b7dfa3b369727a0d5ac4138615985ba3069f
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-06-16 21:41:09 -0600

    Add Binary Search

--- array-adt/array.c
+++ array-adt/array.c
@@ -8,6 +8,14 @@ struct Array
     int length;
 };
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *x;
+    *y = temp;
+}
+
 void Display(struct Array arr) 
 {
     int i;
@@ -60,14 +68,82 @@ int Delete(struct Array *arr, int index)
     return 0;
 }
 
+int LinearSearch(struct Array *arr, int key)
+{
+    int i;
+
+    for (i = 0; i < arr->length; i++) {
+        if (key == arr->A[i])
+            {
+                // swap(&arr->A[i], &arr->A[i-1]); // transposition
+                swap(&arr->A[i], &arr->A[0]); // move to header / front
+                return i;
+            }
+    }
+
+    return -1;
+}
+
+
+int BinarySearch(struct Array arr, int key)
+{
+    int low, mid, high;
+    low = 0;
+    high = arr.length - 1;
+    
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    return -1;     
+}
+
+int RBinSearch(int a[], int low, int high, int key)
+{
+    int mid;
+
+    if (low < high) {
+        mid = (low + high) / 2;
+        if (key == a[mid])
+            return mid;
+        else if (key < a[mid])
+            return RBinSearch(a, low, mid - 1, key);
+        else 
+            return RBinSearch(a, mid + 1, high, key);
+    }
+    
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
     int n, i;
 
+    // Append function
     // Append(&arr, 10);
+
+    //Insert function
     // Insert(&arr, 0, 10);
-    printf("%d\n", Delete(&arr, 4));
+
+    // Delete function
+    // printf("%d\n", Delete(&arr, 4));
+
+    // Linear Search function
+    // printf("%d\n", LinearSearch(&arr, 4));
+
+    // Binary Search function
+    printf("%d\n", BinarySearch(arr, 4));
+
+    // Recursive Binary Search function
+    printf("%d\n", RBinSearch(arr.A, 0, arr.length, 9));
+
     Display(arr);
         
     return 0;
