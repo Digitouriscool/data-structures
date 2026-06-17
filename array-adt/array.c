f2913d5e3c477f3c2f710a30df89d9884abda533
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-06-17 17:54:34 -0600

    Add reverse functions

--- array-adt/array.c
+++ array-adt/array.c
@@ -12,7 +12,7 @@ void swap(int *x, int *y)
 {
     int temp;
     temp = *x;
-    *x = *x;
+    *x = *y;
     *y = temp;
 }
 
@@ -176,6 +176,29 @@ float Avg(struct Array arr)
     return (float) Sum(arr) / arr.length;
 }
 
+void Reverse(struct Array *arr)
+{
+    int *B;
+    int i, j;
+
+    B = (int *) malloc(arr->length * sizeof(int));
+    
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    
+    for (i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+}
+
+void Reverse2(struct Array *arr)
+{
+    int i, j;
+
+    for (i = 0, j = arr->length - 1; i < j; i++, j--) {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
@@ -216,8 +239,13 @@ int main()
     printf("%d\n", Sum(arr));
 
     // Average function
-    printf("%f\n", Sum(arr));
+    printf("%f\n", Avg(arr));
 
+    // Reverse functions
+    // Reverse(&arr);
+    Reverse2(&arr);
+
+    
     Display(arr);
         
     return 0;
