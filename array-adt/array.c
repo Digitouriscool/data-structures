b0a22e14f6c1a97f65ca010a88f3cbd990f93a54
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-06-23 06:56:21 -0600

    Add isSorted and Rearrange functions
    Added isSorted and Rearrange functions to check
    if an array is sorted and to rearrange negative
    elements on the left-hand side and positive
    elements on the right-hand side.

--- array-adt/array.c
+++ array-adt/array.c
@@ -214,9 +214,36 @@ void InsertSort(struct Array *arr, int x)
     arr->length++;
 }
 
+int isSorted(struct Array arr)
+{
+    int i;
+
+    for (i = 0; i < arr.length - 1; i++) {
+        if (arr.A[i] > arr.A[i + 1])
+            return 0;
+    }
+
+    return 1;
+}
+
+void Rearrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j) {
+        while (arr->A[i] < 0) i++;
+        while (arr->A[j] >= 0) j--;
+
+        if (i < j) 
+            swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+    struct Array arr = {{2, -33, 25, 10, -15, -7}, 10, 6};
     int n, i;
 
     // Append function
@@ -262,7 +289,14 @@ int main()
 
     // Insert Sort function
     InsertSort(&arr, 12);
-    
+
+    // Check if array is sorted
+    printf("%d\n", isSorted(arr));
+
+    // Rearrange elements
+    Rearrange(&arr);
+
+    // Display array elements    
     Display(arr);
         
     return 0;
