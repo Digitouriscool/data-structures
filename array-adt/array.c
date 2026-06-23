1a66b392df0fe8461a4ab36fbbdca70526ae4ab7
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-06-23 12:13:20 -0600

    Add Merge function
    The merge function takes two arrays and combines
    them in a sorted manner.

--- array-adt/array.c
+++ array-adt/array.c
@@ -241,6 +241,32 @@ void Rearrange(struct Array *arr)
     }
 }
 
+struct Array* Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array *) malloc((sizeof(struct Array)));
+
+    while(i < arr1->length && j < arr2->length) {
+        if (arr1->A[i] < arr2->A[j]) 
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
     struct Array arr = {{2, -33, 25, 10, -15, -7}, 10, 6};
@@ -296,8 +322,16 @@ int main()
     // Rearrange elements
     Rearrange(&arr);
 
+    // Merge function
+    struct Array arr1 = {{2, 6, 10, 15, 25}, 10, 5};
+    struct Array arr2 = {{3, 4, 7, 18, 20,}, 10, 5};
+    struct Array *arr3;
+
+    arr3 = Merge(&arr1, &arr2);
+    Display(*arr3);
+
     // Display array elements    
-    Display(arr);
+    Display(arr1);
         
     return 0;
 }
