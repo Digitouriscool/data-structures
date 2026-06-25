c5d49aa32983542959c04ece3d6f7338d035fc6a
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2026-06-25 06:28:20 -0600

    Add set union of sorted arrays

--- array-adt/array.c
+++ array-adt/array.c
@@ -267,6 +267,36 @@ struct Array* Merge(struct Array *arr1, struct Array *arr2)
     return arr3;
 }
 
+struct Array* Union(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array *) malloc((sizeof(struct Array)));
+
+    while(i < arr1->length && j < arr2->length) {
+        if (arr1->A[i] < arr2->A[j]) 
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            arr3->A[k++] = arr2->A[j++];
+        else {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
     struct Array arr = {{2, -33, 25, 10, -15, -7}, 10, 6};
@@ -324,12 +354,15 @@ int main()
 
     // Merge function
     struct Array arr1 = {{2, 6, 10, 15, 25}, 10, 5};
-    struct Array arr2 = {{3, 4, 7, 18, 20,}, 10, 5};
+    struct Array arr2 = {{3, 6, 7, 15, 20,}, 10, 5};
     struct Array *arr3;
 
     arr3 = Merge(&arr1, &arr2);
     Display(*arr3);
 
+    // Union of sorted arrays
+    arr3 = Union(&arr1, &arr2);
+    Display(*arr3);
     // Display array elements    
     Display(arr1);
         
